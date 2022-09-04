af7706b1fda4f3d41cc523593268994ce960c3d9
chell2 <rei1259@gmail.com>
2022-09-05 03:06:59 +0900

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+// import '../styles/globals.css'
+// import type { AppProps } from 'next/app'
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import { MantineProvider } from '@mantine/core'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  )
+}
